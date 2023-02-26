273015dabad7e4cb0948e69cff970cad7e687a2d
Krishjampana <123447546+Krishjampana@users.noreply.github.com>
2023-02-25 23:16:43 -0600

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,27 +8,82 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    dts=[]
+    for d in dates:
+        
+        res = datetime.datetime.strptime(d, "%Y-%m-%d").strftime('%d %b %Y')
+        dts.append(res)
+        
+    return dts
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinstance(start, str) or not isinstance(n, int):
+        
+        raise TypeError()
+    
+    dates = []
+    
+    st = datetime.strptime(start, '%Y-%m-%d')
+    
+    for i in range(n):
+        
+        dates.append(st + timedelta(days=i))
+        
+    return dates
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    num_days = len(values)
+    
+    date_range_list = date_range(start_date, num_days)
+
+   
+    ret = list(zip(date_range_list, values))
+    return ret
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    hdrs = ("book_uid,isbn_13,patron_id,date_checkout,date_due,date_returned".
+              split(','))
+    
+    fines = defaultdict(float)
+    
+    with open(infile, 'r') as f:
+        data = DictReader(f, fieldnames=hdrs)
+        rows = [row for row in data]
+
+    rows.pop(0)
+       
+    for line_1 in rows:
+       
+        patronID = line_1['patron_id']
+        
+        date_due = datetime.strptime(line_1['date_due'], "%m/%d/%Y")
+        
+        date_returned = datetime.strptime(line_1['date_returned'], "%m/%d/%Y")
+        
+        late_days = (date_returned - date_due).days
+        
+        fines[patronID]+= 0.25 * late_days if late_days > 0 else 0.0
+        
+                 
+    finalIst = [
+        {'patron_id': p, 'late_fees': f'{f:0.2f}'} for p, f in fines.items()
+    ]
+    with open(outfile, 'w') as f:
+        
+        writer = DictWriter(f,['patron_id', 'late_fees'])
+        writer.writeheader()
+        writer.writerows(finalIst)
 
 
 # The following main selection block will only run when you choose
